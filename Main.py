e6215dd6038c500e22fd8ed4b3bbcac837e2b9c0
TorVik247 <99217595+TorVik247@users.noreply.github.com>
2022-12-17 15:51:22 +0200

    1.1

--- Main.py
+++ Main.py
@@ -1,8 +1,32 @@
 n=8
+x=[]
 x1x2=[]
-i=0
+on=0
+six=0
+i=1
+j=0
 
 while i<(n+1):
-    x1x2.append(i)
+    x.append(i)
     i=i+1
-print(x1x2)
+
+i=0
+while i<n:
+    while  j<n:
+        po=(x[six]**2)+(2*x[on])
+        x1x2.append(po)
+        on =on +1
+        j=j+1
+    on = on - n
+    six =six + 1
+    j=0
+    i=i+1
+
+i=0
+for j in x1x2:   #вивод
+    if i>n-1:
+        print()
+        i=0
+    print(j, end=" ")
+    i=i+1
+
