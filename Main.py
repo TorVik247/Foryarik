998d5efa7deb7d8cb390ed7f8a71a661c0516ec5
TorVik247 <99217595+TorVik247@users.noreply.github.com>
2022-12-18 16:49:58 +0200

    1.2

--- Main.py
+++ Main.py
@@ -1,6 +1,8 @@
 n=8
 x=[]
 x1x2=[]
+pi=[]
+y=[]
 on=0
 six=0
 i=1
@@ -22,6 +24,7 @@ while i<n:#створення таблички 1
     j=0
     i=i+1
 
+
 i=0
 for j in x1x2:   #вивод
     if i>n-1:
@@ -30,3 +33,18 @@ for j in x1x2:   #вивод
     print(j, end=" ")
     i=i+1
 
+t=x1x2[0]
+while t<=po:
+    y.append(t)
+    t=t+1
+print()
+print('y =',y)
+
+i=y[0]
+while i<=len(y):#неправильно
+    pit=i/len(y)
+    pi.append(pit)
+    i=i+1
+
+print()
+print(pi)
