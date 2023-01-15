8cce35b8c02283bb125cb63857f9f0931d8ffe50
TorVik247 <99217595+TorVik247@users.noreply.github.com>
2023-01-16 00:10:30 +0200

    3.0

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+import math
 x=[]
 n=8
 x1x2=[]
@@ -6,6 +7,8 @@ six=0
 pi=[]
 n2=[]
 y=[]
+xyi=[]
+xui=[]
 
 i=1
 j=0
@@ -27,15 +30,15 @@ while i<n:#створення таблички 1
     i=i+1
 
 i=0
-for j in x1x2:   #вивод
-    if i>n-1:
-        print()
-        i=0
-    if j<10:
-        print(j, end="  ")
-    else:
-        print(j, end=" ")
-    i=i+1
+#for j in x1x2:   #вивод
+ #   if i>n-1:
+  #      print()
+   #     i=0
+    #if j<10:
+     #   print(j, end="  ")
+    #else:
+     #   print(j, end=" ")
+    #i=i+1
 
 t=x1x2[0]
 while t<=po:                    #y
@@ -57,8 +60,54 @@ while i<len(y):                 #pi
         pit=j/(n*n)
         pi.append(pit)
         i=i+1
+#print()
+#print()
+#print('y =',y)
+#print('pi=',pi)
+#print('n =', n2)
+
+
+do=0
+i=0
+while n>do:
+    do=do+1
+    i=0
+    doo = 0
+    while i<len(y):
+        if (( ((y[doo]-(do**2))/2) >= 1 ) and ( ((y[doo]-(do**2))/2) <= n ) and ( ((y[doo]-(do**2))/2-int(((y[doo]-(do**2))/2))) == 0 )):
+            xyi.append(1/64)
+        else:
+            xyi.append(0)
+        doo=doo+1
+        i=i+1
+i=0
+for j in xyi:   #вивод
+    if i>len(y)-1:
+        print()
+        i=0
+    else:
+        print(j, end=" ")
+    i=i+1
+
+do=0
+i=0
+while n>do:
+    do=do+1
+    i=0
+    doo = 0
+    while i<len(y):
+        if (( ((y[doo]-(do**2))/2) >= 1 ) and ( ((y[doo]-(do**2))/2) <= n ) and ( ((y[doo]-(do**2))/2-int(((y[doo]-(do**2))/2))) == 0 )):
+            porn=math.log10((y[doo]-(do**2))/2)
+            xui.append(porn)
+        else:
+            xui.append(0)
+        doo=doo+1
+        i=i+1
 print()
-print()
-print('y =',y)
-print('pi=',pi)
-print('n =', n2)
+for j in xui:   #вивод
+    if i>len(y)-1:
+        print()
+        i=0
+    else:
+        print(j, end=" ")
+    i=i+1
