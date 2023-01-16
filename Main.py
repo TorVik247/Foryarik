d536cde275c60c4ebd837f0198c75d6a61d90449
TorVik247 <99217595+TorVik247@users.noreply.github.com>
2023-01-16 20:57:46 +0200

    last

--- Main.py
+++ Main.py
@@ -30,15 +30,15 @@ while i<n:#створення таблички 1
     i=i+1
 
 i=0
-#for j in x1x2:   #вивод
- #   if i>n-1:
-  #      print()
-   #     i=0
-    #if j<10:
-     #   print(j, end="  ")
-    #else:
-     #   print(j, end=" ")
-    #i=i+1
+for j in x1x2:   #вивод
+    if i>n-1:
+        print()
+        i=0
+    if j<10:
+        print(j, end="  ")
+    else:
+        print(j, end=" ")
+    i=i+1
 
 t=x1x2[0]
 while t<=po:                    #y
@@ -60,12 +60,13 @@ while i<len(y):                 #pi
         pit=j/(n*n)
         pi.append(pit)
         i=i+1
-#print()
-#print()
-#print('y =',y)
-#print('pi=',pi)
-#print('n =', n2)
-
+print()
+print()
+print('y =',y)
+print('pi=',pi)
+print('n =', n2)
+print()
+print()
 
 do=0
 i=0
@@ -96,13 +97,19 @@ while n>do:
     i=0
     doo = 0
     while i<len(y):
-        if (( ((y[doo]-(do**2))/2) >= 1 ) and ( ((y[doo]-(do**2))/2) <= n ) and ( ((y[doo]-(do**2))/2-int(((y[doo]-(do**2))/2))) == 0 )):
-            porn=math.log10((y[doo]-(do**2))/2)
-            xui.append(porn)
+        if ((((y[doo]-(do**2))/2) >= 1) and (((y[doo]-(do**2))/2) <= n) and (((y[doo]-(do**2))/2-int(((y[doo]-(do**2))/2))) == 0)):
+            porn=math.log10(((y[doo]-(do**2))/2))
+            if porn==0:
+                xui.append(1/64)
+            else:
+                xui.append(porn)
         else:
             xui.append(0)
         doo=doo+1
         i=i+1
+print()
+
+i=0
 print()
 for j in xui:   #вивод
     if i>len(y)-1:
